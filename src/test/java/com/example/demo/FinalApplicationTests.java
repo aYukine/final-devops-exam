dce5389c4eeb03ab8db6cc7fec330359ff30c2e8
aYukine <phayuk168@gmail.com>
2026-06-17 19:54:59 +0700

    test: intentional fail to test email notification

--- src/test/java/com/example/demo/FinalApplicationTests.java
+++ src/test/java/com/example/demo/FinalApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,9 @@ class FinalApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	void failingTest() {
+        assertEquals(1, 2);
+    }
 
 }
